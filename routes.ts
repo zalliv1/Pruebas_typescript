e5929463203eaf57578df79e50c6e88df57a1af1
zalliv1 <50331824+zalliv1@users.noreply.github.com>
2025-10-04 16:14:12 -0500

    Update routes.ts

--- routes.ts
+++ routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { addBusinessTime } from "./utils/businessDays";
+import { addBusinessTime } from "./utils/businessDays.ts";
 
 export const router = Router();
 
